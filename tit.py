de19953e6090b54c6db92b159aac7bacc67e9eee
yangychen <yangychen@ebay.com>
2013-10-29 10:16:20 +0800

    Add feature that tit.py can load files.

--- tit.py
+++ tit.py
@@ -5,5 +5,16 @@ from interp.tilib import dostring
 from interp.interp import Interpreter
 from tieslib.tieslib import prompt, setup_ties_environment
 
+
+def setup_tit_environment():
+    global_env = setup_ties_environment()
+    import sys
+    if len(sys.argv) > 1:
+        from interp.tilib import dofile
+        fns = sys.argv[1:]
+        for fn in fns:
+            dofile(fn, global_env)
+    return global_env
+
 driver_loop(lambda: '%s > ' % prompt(),
-            Interpreter(dostring, setup_ties_environment))
+            Interpreter(dostring, setup_tit_environment))

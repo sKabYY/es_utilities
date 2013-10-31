df07bf75d712954a89b05e325d465393c2ea761b
yangychen <yangychen@ebay.com>
2013-10-31 15:20:03 +0800

    Add history file

--- tiesh.py
+++ tiesh.py
@@ -17,5 +17,25 @@ def yellow(s):
 def input_prompt():
     return yellow('%s\n> ' % prompt())
 
+### Load and save history file ############################
+import os
+import readline
+histfile = os.path.join(os.path.expanduser('~'), '.ties_history')
+try:
+    readline.read_history_file(histfile)
+except IOError:
+    pass
+import atexit
+
+
+def write_history():
+    try:
+        readline.write_history_file(histfile)
+    except IOError:
+        pass
+
+atexit.register(write_history)
+###########################################################
+
 newenv = newenv_with_preload(setup_ties_environment, sys.argv[1:])
 driver_loop(newenv, input_prompt)
